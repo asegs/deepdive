83a5c7944f689f3d9334cd2b197b536e9067021c
Aaron <aarsegal@gmail.com>
2025-05-19 17:04:16 -0400

    Initial commit, adds basic html visualizer and recursive script

---
+++ examples/lansing_school.js
@@ -0,0 +1,345 @@
+export const Lansing = {
+  "GENERAL SUPPORT": {
+    "BOARD OF EDUCATION": {
+      "BOARD OF EDUCATION": {
+        "A-1010-16 Noninstructional Salaries ": 2000,
+        "A-1010-400 CONTRACTUAL ": 8000,
+        "A-1010-450 Materials & Supplies ": 4000,
+        "A-1010-479 BOE member conferences ": 9000,
+        "A-1010-490 BOCES Services ": 17006,
+        "total": 40006
+      },
+      "DISTRICT CLERK": {
+        "A-1040-450 Materials & Supplies ": 2000,
+        "A-1040-479 Conference/Training ": 500,
+        "total": 2500
+      },
+      "DISTRICT MEETING": {
+        "A-1060-400 CONTRACTUAL ": 1000,
+        "A-1060-450 Materials & Supplies ": 4000,
+        "total": 5000
+      },
+      "total": 47506
+    },
+    "CENTRAL ADMINISTRATION": {
+      "CHIEF SCHOOL ADMINISTRATOR": {
+        "A-1240-15 Instructional Salaries ": 214392,
+        "A-1240-16 Noninstructional Salaries ": 86232,
+        "A-1240-400 CONTRACTUAL ": 6000,
+        "A-1240-450 Materials & Supplies ": 5000,
+        "A-1240-479 Tchr Conference ": 5000,
+        "total": 316624
+      },
+      "total": 316624
+    },
+    "FINANCE": {
+      "BUSINESS ADMINISTRATION": {
+        "A-1310-15 Instructional Salaries ": 140236,
+        "A-1310-16 Noninstructional Salaries ": 157494,
+        "A-1310-400 CONTRACUAL ": 4000,
+        "A-1310-450 Materials & Supplies ": 3000,
+        "A-1310-473 Postage ": 0,
+        "A-1310-479 Tchr Conference ": 5000,
+        "A-1310-490 BOCES Services ": 184843,
+        "total": 494573
+      },
+      "AUDITING": {
+        "A-1320-400 CONTRACTUAL ": 35000,
+        "total": 35000
+      },
+      "TREASURER": {
+        "A-1325-16 Noninstructional Salaries ": 74674,
+        "A-1325-400 CONTRACTUAL ": 8000,
+        "A-1325-479 Treas Conference ": 500,
+        "total": 83174
+      },
+      "TAX COLLECTOR": {
+        "A-1330-400 TAX COLLECTION ": 6000,
+        "total": 6000
+      },
+      "total": 618747
+    },
+    "STAFF": {
+      "LEGAL": {
+        "A-1420-400 CONTRACTUAL ": 80000,
+        "total": 80000
+      },
+      "PERSONNEL": {
+        "A-1430-15 Instructional Salaries - ": 2000,
+        "A-1430-400 CONTRACTUAL ": 6000,
+        "A-1430-450 Materials & Supplies ": 1500,
+        "A-1430-490 BOCES Services ": 9284,
+        "total": 18784
+      },
+      "PUBLIC INFORMATION AND SERVICES": {
+        "A-1480-400 Public Information ": 6000,
+        "A-1480-450 Materials & Supplies ": 300,
+        "A-1480-490 BOCES Services ": 25000,
+        "total": 31300
+      },
+      "total": 130084
+    },
+    "CENTRAL SERVICES": {
+      "OPERATION OF PLANT": {
+        "A-1620-16 Noninstructional Salaries ": 596342,
+        "A-1620-200 Equipment ": 68800,
+        "A-1620-400 CONTRACTUAL ": 195000,
+        "A-1620-421 Fuel Oil Grounds ": 2500,
+        "A-1620-422 Gas ": 116000,
+        "A-1620-425 Electric ": 233600,
+        "A-1620-426 Water ": 43000,
+        "A-1620-450 Materials & Supplies ": 145000,
+        "A-1620-479 Conference ": 1000,
+        "A-1620-490 BOCES Services ": 110891,
+        "total": 1512133
+      },
+      "MAINTENANCE OF PLANT": {
+        "A-1621-16 Noninstructional Salaries ": 221123,
+        "A-1621-200 Equipment ": 60000,
+        "A-1621-400 GROUNDS CONTRACTUAL ": 50000,
+        "A-1621-450 Materials & Supplies ": 70000,
+        "total": 401123
+      },
+      "CENTRAL PRINTING & MAILING": {
+        "A-1670-400 CONTRACTUAL-Postage ": 23000,
+        "A-1670-405 CONTR-Copier Contracts ": 5000,
+        "A-1670-455 CONTR-Copier Supplies ": 500,
+        "A-1670-490 BOCES Central Printing & M ": 88450,
+        "total": 116950
+      },
+      "CENTRAL DATA PROCESSING": {
+        "A-1680-450 Materials & Supplies ": 0,
+        "A-1680-490 BOCES Services ": 102997,
+        "total": 102997
+      },
+      "total": 2133204
+    },
+    "SPECIAL ITEMS (CONTRACTUAL EXPENSES)": {
+      "UNALLOCATED INSURANCE A-1910-400 CONTRACTUAL ": 145000,
+      "SCHOOL ASSOCIATION DUES A-1920-400 CONTRACTUAL ": 10000,
+      "BOCES ADMINISTRATIVE COSTS A-1981-490 BOCES Services ": 652483,
+      "CONSULTANT FEES A-1989-400 CONSULTANT FEES ": 2500,
+      "total": 809983
+    },
+    "total": 4056148
+  },
+  "INSTRUCTION": {
+    "ADMINISTRATION AND IMPROVEMENT": {
+      "CURRICULUM DEVEL AND SUPRVSN": {
+        "A-2010-15 Instructional Salaries ": 129556,
+        "A-2010-400 CONTRACTUAL ": 500,
+        "A-2010-450 Materials & Supplies ": 2000,
+        "A-2010-479 Tchr Conference ": 15000,
+        "A-2010-490 BOCES Services ": 15235,
+        "total": 162291
+      },
+      "SUPERVISION-REGULAR SCHOOL": {
+        "A-2020-15 Instructional Salaries - ": 622313,
+        "A-2020-16 Noninstructional Salaries ": 202126,
+        "A-2020-200 Equipment ": 1500,
+        "A-2020-400 CONTRACTUAL ": 2089,
+        "A-2020-450 Materials & Supplies ": 22150,
+        "A-2020-479 Tchr Conference ": 0,
+        "total": 850178
+      },
+      "RESEARCH, PLANNING & EVALUATION": {
+        "A-2060-490 BOCES Services ": 0,
+        "total": 0
+      },
+      "INSERVICE TRAINING-INSTRUCTION": {
+        "A-2070-15 Instructional Salaries ": 0,
+        "A-2070-400 CONTRACTUAL ": 2500,
+        "A-2070-450 Materials & Supplies ": 500,
+        "A-2070-490 BOCES Services ": 161342,
+        "total": 164342
+      },
+      "total": 1176812
+    },
+    "TEACHING": {
+      "REGULAR SCHOOL": {
+        "A-2110-12 Teacher Salaries K-6 ": 3469919,
+        "A-2110-13 Teacher Salaries 7-12 ": 4000043,
+        "A-2110-13 Tchr Sal-Tchr Assistant ": 286578,
+        "A-2110-14 Teacher Substitutes ": 262000,
+        "A-2110-15 Tchr Sal - Curriculum ": 118000,
+        "A-2110-15 Mentors ": 12500,
+        "A-2110-16 Noninstructional Salaries ": 196040,
+        "A-2110-200 Equipment ": 3500,
+        "A-2110-400 CONTRACTUAL ": 29338,
+        "A-2110-450 Materials & Supplies ": 153019,
+        "A-2110-473 Pymt to Charter Schools ": 75000,
+        "A-2110-479 Tchr Conference ": 12975,
+        "A-2110-480 Textbooks ": 81410,
+        "A-2110-490 BOCES Services ": 758169,
+        "total": 9458491
+      },
+      "PRG FOR SDNTS W/DISABIL-MED ELGBLE": {
+        "A-2250-13 Teacher Asst Spec Ed ": 373009,
+        "A-2250-15 Instructional Salaries ": 1419284,
+        "A-2250-16 Noninstructional Salaries ": 750525,
+        "A-2250-200 Equipment ": 750,
+        "A-2250-400 CONTRACTUAL ": 286000,
+        "A-2250-450 Materials & Supplies ": 10000,
+        "A-2250-471 Tuition Pd - District NYS ": 175000,
+        "A-2250-472 Tuition-High Cost ": 190000,
+        "A-2250-473 Pymt to Charter Schools ": 15000,
+        "A-2250-479 Tchr Conference ": 1500,
+        "A-2250-480 Textbooks ": 2500,
+        "A-2250-490 BOCES Services ": 925706,
+        "total": 4149273
+      },
+      "PRG FOR ENGLISH LANGUAGE LEARNERS": {
+        "A-2259-15 Instructional Salaries - ": 258206,
+        "A-2259-450 Materials & Supplies ": 1000,
+        "A-2259-490 BOCES Services ": 0,
+        "total": 259206
+      },
+      "OCCUPATIONAL ED (GRADES 9-12)": {
+        "A-2280-490 BOCES Services ": 1271711,
+        "total": 1271711
+      },
+      "TEACHING-SPECIAL SCHOOLS": {
+        "A-2330-15 Instr Sal - Summer School ": 60598,
+        "A-2330-450 Mat & Sup - Incar Yth ": 500,
+        "total": 61098
+      },
+      "total": 15199779
+    },
+    "INSTRUCTIONAL MEDIA": {
+      "SCHOOL LIBRARY AND AV": {
+        "A-2610-15 Instructional Salaries ": 166961,
+        "A-2610-16 Noninstructional Salaries ": 54325,
+        "A-2610-200 Equipment ": 500,
+        "A-2610-400 CONTRACTUAL ": 850,
+        "A-2610-450 Materials & Supplies ": 4792,
+        "A-2610-460 LIBRARY BOOKS ": 14000,
+        "A-2610-490 BOCES Services ": 51116,
+        "total": 292544
+      },
+      "COMPUTER ASSISTED INSTRUCTION": {
+        "A-2630-16 Noninstructional Salaries ": 239680,
+        "A-2630-222 State-Aided Comp Hardwa ": 15750,
+        "A-2630-400 CONTRACTUAL ": 8000,
+        "A-2630-450 Materials & Supplies ": 7000,
+        "A-2630-460 State-Aided Comp Softwar ": 16500,
+        "A-2630-479 Tchr Conference ": 1500,
+        "A-2630-490 BOCES Services ": 704442,
+        "total": 992872
+      },
+      "total": 1285416
+    },
+    "PUPIL SERVICES": {
+      "ATTENDANCE-REGULAR SCHOOL": {
+        "A-2805-16 Noninstructional Salaries ": 55272,
+        "A-2805-450 Materials & Supplies ": 1000,
+        "total": 56272
+      },
+      "GUIDANCE-REGULAR SCHOOL": {
+        "A-2810-15 Instructional Salaries ": 251122,
+        "A-2810-16 Noninstructional Salaries ": 50444,
+        "A-2810-400 CONTRACTUAL ": 1800,
+        "A-2810-450 ": 20000,
+        "A-2810-490 BOCES Services ": 8558,
+        "total": 331924
+      },
+      "HEALTH SRVCS-REGULAR SCHOOL": {
+        "A-2815-16 Noninstructional Salaries ": 178320,
+        "A-2815-200 Equipment ": 500,
+        "A-2815-400 CONTRACTUAL ": 770,
+        "A-2815-401 Private School Hlth Svcs ": 46000,
+        "Materials & Supplies ": 3907,
+        "A-2815-490 BOCES ": 52798,
+        "total": 282295
+      },
+      "PSYCHOLOGICAL SRVCS-REG SCHL": {
+        "A-2820-15 Instructional Salaries ": 378722,
+        "A-2820-450 Materials & Supplies ": 750,
+        "total": 379472
+      },
+      "SOCIAL WORK SRVCS-REGULAR SCHOOL": {
+        "A-2825-15 Instructional Salaries ": 229964,
+        "total": 229964
+      },
+      "CO-CURRICULAR ACTIV-REG SCHL": {
+        "A-2850-15 Instructional Salaries ": 150000,
+        "A-2850-16 Noninstructional Salaries ": 0,
+        "A-2850-400 CONTRACTUAL ": 0,
+        "total": 150000
+      },
+      "INTERSCHOLASTIC ATHLETICS-REG SCHL": {
+        "A-2855-15 ": 320748,
+        "A-2855-16 Noninstructional Salaries ": 22150,
+        "A-2855-200 Equipment ": 38000,
+        "A-2855-400 CONTRACTUAL ": 91000,
+        "A-2855-450 Materials & Supplies ": 25000,
+        "A-2855-479 Conference ": 12000,
+        "A-2855-490 BOCES Services ": 42202,
+        "total": 551099
+      },
+      "total": 1981027
+    },
+    "total": 19643033
+  },
+  "PUPIL TRANSPORTATION": {
+    "PUPIL TRANSPORTATION": {
+      "DISTRICT TRANS. SRVICES": {
+        "A-5510-16 ": 954406,
+        "A-5510-200 Equipment ": 42000,
+        "A-5510-400 CONTRACTUAL ": 47000,
+        "A-5510-404 Bus Repairs ": 15000,
+        "A-5510-410 Insurance ": 35000,
+        "A-5510-450 Materials & Supplies ": 20000,
+        "A-5510-479 Professional Conference ": 5000,
+        "A-5510-490 BOCES Services ": 16613,
+        "A-5510-570 Auto Supplies ": 1000,
+        "A-5510-571 Gas & Diesel - District ": 150000,
+        "A-5510-572 Oil & Lubricants ": 8000,
+        "A-5510-573 Tires ": 14000,
+        "A-5510-574 Bus Parts ": 90000,
+        "total": 1398019
+      },
+      "GARAGE BUILDING": {
+        "A-5530-200 Equipment ": 0,
+        "A-5530-400 CONTRACTUAL ": 2000,
+        "A-5530-422 Gas ": 10000,
+        "A-5530-423 Uniforms ": 1200,
+        "A-5530-425 Electric ": 10000,
+        "A-5530-450 Materials & Supplies ": 1500,
+        "total": 24700
+      },
+      "total": 1422719
+    },
+    "total": 1422719
+  },
+  "UNDISTRIBUTED": {
+    "EMPLOYEE BENEFITS": {
+      "STATE RETIREMENT A-9010-800 Empl Retirement - Distric ": 504101,
+      "TEACHERS' RETIREMENT A-9020-800 Teachers' Retirement ": 1249128,
+      "SOCIAL SECURITY A-9030-800 SS/Medicare - District ": 1245259,
+      "WORKERS' COMPENSATION A-9040-800 Workers' Comp. ": 65000,
+      "UNEMPLOYMENT INSURANCE A-9050-800 Unemployment Insurance ": 20000,
+      "HOSP, MEDICAL, DENTAL INSURANCE A-9060-80 Hosp, Medical, Dental Ins ": 7876376,
+      "MEDICARE REIMB A-9060-852 Medicare Reimbursement ": 90000,
+      "OTHER BENEFIT A-9089-800 OTHER EMPLOYEE BENEFIT ": 100000,
+      "total": 11149863
+    },
+    "DEBT SERVICE": {
+      "A-9711-600 Serial Bond - Constr-Prin ": 1390000,
+      "A-9711-700 Serial Bond - Constr- Int ": 574400,
+      "A-9712-600 Serial Bond - EPC- Princ ": 265000,
+      "A-9712-700 Serial Bond - EPC- Int ": 13701,
+      "A-9722-600 Stat Bond Bus Purch - Pr ": 355000,
+      "A-9722-700 Stat Bond Bus Purch - Int ": 30463,
+      "A-9731-600 BAN - Construction Princ ": 220000,
+      "A-9731-700 BAN - Construction Int ": 453298,
+      "total": 3301862
+    },
+    "INTERFUND TRANSFERS": {
+      "A-9901-950 Transfer to Special Aid ": 22000,
+      "A-9950-900 Transfer to Capital Fund ": 100000,
+      "total": 122000
+    },
+    "total": 14573725
+  },
+  "total": 39695625
+}
---
+++ visualize.js
@@ -0,0 +1,53 @@
+import {Lansing} from "./examples/lansing_school.js";
+
+const ctx = document.getElementById('myChart');
+let currentPath = []
+const staticData = Lansing;
+let currentChart;
+
+function createNewChart(data) {
+    const withoutTotal = Object.fromEntries(Object.entries(data).filter(d => d[0] !== 'total'));
+    return new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: Object.keys(withoutTotal),
+            datasets: [{
+                label: '$ spent',
+                data: Object.values(withoutTotal).map(v => typeof v === 'object' ? v['total'] : v),
+                borderWidth: 1
+            }]
+        },
+        options: {
+            'onClick': (e, elems, chart) => {
+                chart.destroy()
+                // Bad behavior
+                const idx = elems[0].index;
+                const name = Object.keys(withoutTotal)[idx]
+                currentPath.push(name)
+                let view = staticData;
+                console.log(view)
+                console.log(currentPath)
+                for (const token of currentPath) {
+                    console.log(token)
+                    view = view[token]
+                    console.log(view)
+                }
+                currentChart = createNewChart(view)
+            }
+        },
+    })
+}
+
+export function goBack() {
+    currentPath.pop()
+    let view = staticData;
+    for (const token of currentPath) {
+        view = staticData[token];
+    }
+    currentChart.destroy()
+    currentChart = createNewChart(view)
+}
+
+document.getElementById("back").onclick = goBack
+
+currentChart = createNewChart(staticData)
