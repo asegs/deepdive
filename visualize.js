8a9c4b7b580934d08bab637c683fe7854a49c812
Aaron <aarsegal@gmail.com>
2025-05-19 20:16:56 -0400

    Added labels

--- visualize.js
+++ visualize.js
@@ -1,5 +1,7 @@
 import {Lansing} from "./examples/lansing_school.js";
 
+const normalColors = ['yellow', 'blue', 'red', 'orange', 'purple', 'gold']
+
 const ctx = document.getElementById('myChart');
 let currentPath = []
 const staticData = Lansing;
@@ -14,7 +16,9 @@ function createNewChart(data) {
             datasets: [{
                 label: '$ spent',
                 data: Object.values(withoutTotal).map(v => typeof v === 'object' ? v['total'] : v),
-                borderWidth: 1
+                borderWidth: 2,
+                borderColor: Object.values(withoutTotal).map(v => typeof v === 'object' ? 'green' : 'gray'),
+                backgroundColor: new Array(Object.keys(withoutTotal).length).fill(0).map((_, i) => normalColors[i % normalColors.length])
             }]
         },
         options: {
@@ -24,6 +28,7 @@ function createNewChart(data) {
                 const idx = elems[0].index;
                 const name = Object.keys(withoutTotal)[idx]
                 currentPath.push(name)
+                document.getElementById("path").innerText = currentPath.join('.')
                 let view = staticData;
                 for (const token of currentPath) {
                     view = view[token]
@@ -36,6 +41,7 @@ function createNewChart(data) {
 
 export function goBack() {
     currentPath.pop()
+    document.getElementById("path").innerText = currentPath.join('.')
     let view= staticData;
     for (const token of currentPath) {
         view = view[token];
